519b7fcabeff650bbc8787f11960fb422610c758
Robert H <robertjameshardin@aol.com>
2019-02-10 19:12:40 -0600

    base game finally working, text not changing properly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,67 @@
 //Variables for win, loss, and guesses
-var wins = 0;
-var losses = 0;
-var guessesRemaining = 10;
+let wins = 0;
+let losses = 0;
+let guessesRemaining = 10;
 //Array for computer's pick
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // Variables that correspond to HTML
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
-var remainingText = document.getElementById("guesses-remaining");
-var guessesText = document.getElementById("guesses-text");
-var instructionsText = document.getElementById("instructions-text");
-var guessesArray = [];
+let winsText = document.getElementById("wins-text");
+let lossesText = document.getElementById("losses-text");
+let remainingText = document.getElementById("guesses-remaining");
+let guessesText = document.getElementById("guesses-text");
+let instructionsText = document.getElementById("instructions-text");
+let guessesArray = [];
+let compGuess = [];
 
-//Function for key up event
-document.onkeyup = function() {
+//Initial function for computer to choose a random letter
+window.onload = function() {
+let computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+compGuess.push(computerGuess);
+    console.log(computerGuess[0]);
+}
 
-    // Records keyup as a guess
-    var userGuess = event.key;
-    console.log(userGuess);
 
-    //randomizes computer guess from computerChoices array
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    
-    
-        if (userGuess === computerGuess) {
+//Main Game function, captures key up and affects each html component based on computerGuess
+document.onkeyup = function(event) {
+    const userGuess = event.key;
+    guessesArray.push(userGuess);
+    console.log(compGuess[0]);
+
+        if ((userGuess === compGuess[0]) && (guessesRemaining > 0)) {
             wins++;
-        } else if (guessesRemaining === 0) {
-            losses++;
-        } else if (userGuess !== computerGuess) {
-            guessesRemaining--;
-            instructionsText.textContent = 'Wrong, guess again!'
+            guessesRemaining = 9;
+            guessesArray.length = 0;
+            compGuess.length = 0;
+            let computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+            compGuess.push(computerGuess[0]);
+            console.log(compGuess[0]);
+        } else if ((userGuess !== compGuess[0]) && (guessesRemaining > 0)) {
+            guessesRemaining = guessesRemaining-1;
+            instructionsText.textContent = "Wrong, guess again!";
         } else {
-          instructionsText.textContent = "That's not a letter.."
+            losses++;
+            guessesRemaining = 9;
+            guessesArray.length = 0;
+            compGuess.length = 0;
+            let computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+            compGuess.push(computerGuess[0]);
+            console.log(compGuess[0]);
         }
-    
-
-    //Variable to avoid redundancy
-    if (userGuess === guessesText) {
-        document.removeEventListener('keyup');
-    }
+        
+        
     //Change the directions to "what letter am I thinking of"
     instructionsText.textContent = "Guess what letter I'm thinking of!";
     //Showing the text of wins, losses, remaining, etc with the value of each on the page
     winsText.textContent = "Wins: " + wins;
     lossesText.textContent = "Losses: " + losses;
     remainingText.textContent = "Guesses remaining: " + guessesRemaining;
-    guessesText.textContent = "Letters Guessed:" + userGuess;
+    guessesText.textContent = "Letters Guessed:" + guessesArray;
 
 }
 
-//Another function for avoiding constant restarting of game
-function  checkStatus() {
-    if ((wins === +1) || (losses > 0)) {
-        psychicGame();
-    } 
-}
+
+
+
 
 
 /* Pseudo-Code
@@ -63,5 +70,5 @@ function  checkStatus() {
 /3. User has 9 guesses at the beginning of round to guess the letter {
 /4. If key pressed is incorrect, letter is displayed in text area {
 /5. else if guesses remaining = 0, user loses the round.
-6. Make game continue until correct letter is guessed.
+/6. Make letter choice and game continue until correct letter is guessed.
 */
